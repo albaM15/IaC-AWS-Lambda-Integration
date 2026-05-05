5b998215348a47085fd6ba3c4bb5fbf03ea9d94f
alba <albamalca15@gmail.com>
2026-05-05 14:59:49 -0500

    chore: comentar outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,14 @@
-output "api_endpoint" {
-  description = "API Gateway endpoint URL"
-  value       = "${aws_apigatewayv2_api.http_api.api_endpoint}/upload"
-}
+# output "api_endpoint" {
+#   description = "API Gateway endpoint URL"
+#   value       = "${aws_apigatewayv2_api.http_api.api_endpoint}/upload"
+# }
 
-output "s3_bucket_name" {
-  description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.images.bucket
-}
+# output "s3_bucket_name" {
+#   description = "Name of the S3 bucket"
+#   value       = aws_s3_bucket.images.bucket
+# }
 
-output "sqs_queue_url" {
-  description = "URL of the main SQS queue"
-  value       = aws_sqs_queue.main.url
-}
+# output "sqs_queue_url" {
+#   description = "URL of the main SQS queue"
+#   value       = aws_sqs_queue.main.url
+# }
